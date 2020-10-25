527a767136c70db83adc6b1f07ebe31ee676a06d
Eduarda Oliveira Filadelfo <eolivefi@everis.com>
2020-10-25 03:00:03 -0300

    Inicio do programa

---
+++ src/exercicioLogica1/LogicaProg.java
@@ -0,0 +1,24 @@
+package exercicioLogica1;
+
+import java.util.Scanner;
+
+public class LogicaProg {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Digite a quantidade de meses que deseja calcular: ");
+			
+		Scanner entrada = new Scanner(System.in);
+			
+		int meses = entrada.nextInt();
+		
+		int resultado = meses * 30;
+		
+		System.out.println("A quantidade de dias em "+meses+ " meses é de: "+resultado+ " dias");
+		
+		
+		
+	}
+}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ELProgama1 {
+}
